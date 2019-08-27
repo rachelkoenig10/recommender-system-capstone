2e3d4b7d2de34340b0763eed93d1a0f488bc9ad4
Rachel Koenig <rmdickinson10@gmail.com>
2019-08-26 19:09:09 -0700

    recommender updates

--- Flask app/recommender.py
+++ Flask app/recommender.py
@@ -4,25 +4,12 @@ import pandas as pd
 import pickle
 from flask import Flask, request, render_template, jsonify
 # read in recommender df
-recommender_df = pd.read_csv('data/recommender.csv').set_index('name_x')
+recommender_df = pd.read_csv('../data/tfidf_recommender.csv').set_index('name_x')
 # initialize the flask app
 app = Flask('myApp')
 ### route 1: hello world
 # define the route
 @app.route('/')
-# create the controller
-def home():
-    # return the view
-    return 'Welcome!'
-
-@app.route('/hc_page')
-
-def hc_page():
-    # return the view
-    return '<html><body><h1>Select one of the below products to see recommendations</h1><p>Copy and Paste into search box.</p></body></html>'
-
-# define the route
-@app.route('/form')
 # create the controller
 def form():
     #return the view
@@ -33,7 +20,7 @@ def form():
 def submit():
     user_input = request.args
     # get user input from dict
-    product = user_input['ProductName']
+    product = user_input["ProductName"]
     # run sort values code
     results = list(recommender_df[product].sort_values()[1:11].index)
 
